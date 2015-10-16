edabaeb0166d6878bd6a77771c0fb141def3d486
Pedram Veisi <pedramveisi@gmail.com>
2015-10-15 21:53:43 -0600

    Create WidgetProvider class

---
+++ FootballScores/app/src/main/java/barqsoft/footballscores/widget/WidgetProvider.java
@@ -0,0 +1,9 @@
+package barqsoft.footballscores.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by pedram on 15/10/15.
+ */
+public class WidgetProvider extends AppWidgetProvider {
+}
