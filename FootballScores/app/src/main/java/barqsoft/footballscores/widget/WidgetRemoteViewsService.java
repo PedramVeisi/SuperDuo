82257cd91e14bb323d68b7cfd1b38e81b6d51002
Pedram Veisi <pedramveisi@gmail.com>
2015-10-16 22:29:34 -0600

    Create WidgetRemoteViewsService

---
+++ FootballScores/app/src/main/java/barqsoft/footballscores/widget/WidgetRemoteViewsService.java
@@ -0,0 +1,14 @@
+package barqsoft.footballscores.widget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+/**
+ * Created by pedram on 16/10/15.
+ */
+public class WidgetRemoteViewsService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return null;
+    }
+}
